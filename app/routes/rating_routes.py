520b9238ba7e1946578fbfefbbe44c7ba298ea78
YOUR_USERNAME <daniel_piciocchi@outlook.com>
2023-03-17 11:35:40 +1100

    comments in rating routes

--- app/routes/rating_routes.py
+++ app/routes/rating_routes.py
@@ -7,6 +7,8 @@ rating_bp = Blueprint("rating_bp", __name__)
 rating_schema = RatingSchema()
 ratings_schema = RatingSchema(many=True)
 
+# Add a new rating
+
 
 @app.route('/api/ratings', methods=['POST'])
 @jwt_required()
@@ -18,31 +20,42 @@ def add_rating():
     if not podcast_id or rating_value is None:
         return make_response(jsonify({"message": "Missing required data"}), 400)
 
-    new_rating = Rating(user_id=user_id, podcast_id=podcast_id, rating=rating_value)
+    # Create a new rating and add it to the database
+    new_rating = Rating(
+        user_id=user_id, podcast_id=podcast_id, rating=rating_value)
     db.session.add(new_rating)
     db.session.commit()
 
     return make_response(jsonify({"message": "Rating added successfully"}), 201)
 
+# Get all ratings
+
 
 @app.route('/api/ratings', methods=['GET'])
 def get_all_ratings():
+    # Query the database for all Rating records
     ratings = Rating.query.all()
     result = ratings_schema.dump(ratings)
     return make_response(jsonify(result), 200)
 
+# Get a specific rating
+
 
 @app.route('/api/ratings/<int:rating_id>', methods=['GET'])
 def get_rating(rating_id):
+    # Query the database for a specific Rating record by its ID
     rating = Rating.query.get_or_404(rating_id)
     result = rating_schema.dump(rating)
     return make_response(jsonify(result), 200)
+
+# Update a specific rating
 
 
 @app.route('/api/ratings/<int:rating_id>', methods=['PUT'])
 @jwt_required()
 def update_rating(rating_id):
     user_id = get_jwt_identity()
+    # Query the database for a specific Rating record by its ID
     rating = Rating.query.get_or_404(rating_id)
 
     if rating.user_id != user_id:
@@ -53,21 +66,26 @@ def update_rating(rating_id):
     if rating_value is None:
         return make_response(jsonify({"message": "Missing required data"}), 400)
 
+    # Update the rating value and save it to the database
     rating.rating = rating_value
     db.session.commit()
 
     return make_response(jsonify({"message": "Rating updated successfully"}), 200)
+
+# Delete a specific rating
 
 
 @app.route('/api/ratings/<int:rating_id>', methods=['DELETE'])
 @jwt_required()
 def delete_rating(rating_id):
     user_id = get_jwt_identity()
+    # Query the database for a specific Rating record by its ID
     rating = Rating.query.get_or_404(rating_id)
 
     if rating.user_id != user_id:
         return make_response(jsonify({"message": "Unauthorized action"}), 403)
 
+    # Delete the Rating record from the database
     db.session.delete(rating)
     db.session.commit()
 
